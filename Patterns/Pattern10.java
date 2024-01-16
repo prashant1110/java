5cc2f2d0692c32dffb1fa30d53ff76ac3c69a3bd
Prashant Singh <prashant.singh.vaishali@gmail.com>
2024-01-16 20:23:59 +0530

    patterns

--- Patterns/Pattern10.java
+++ Patterns/Pattern10.java
@@ -1,17 +1,29 @@
 class InnerPattern10 {
-    public static void test(int n){
+    public static void patterns(int n){
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
-                System.out.println("*");
+                System.out.print("*");
             }
             System.out.println();
         }
     }
+
+    public static void invertedPatterns(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = n; j <= n; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+
 }
 
 public class Pattern10 {
     public static void main(String[] args) {
         InnerPattern10 p=new InnerPattern10();
-        p.test(4);
+        p.patterns(4);
+        p.invertedPatterns(4);
     }
 }
