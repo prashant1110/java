85d014bf97780c3eb3d6b00dedf92ccb42bfe9f1
Prashant Singh <prashant@codetailor.in>
2024-01-18 16:52:30 +0530

    pattern10

--- Patterns/Pattern10.java
+++ Patterns/Pattern10.java
@@ -1,29 +1,25 @@
 class InnerPattern10 {
-    public static void patterns(int n){
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
+    public static void patterns(int n) {
+        for (int i = 1; i <= 2 * n; i++) {
+
+            int stars = i;
+
+            if (i > n)
+                stars = 2 * n - i;
+
+            for (int j = 1; j <= stars; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
-
-    public static void invertedPatterns(int n){
-        for (int i = 0; i < n; i++) {
-            for (int j = n; j <= n; j--) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-
 
 }
 
 public class Pattern10 {
     public static void main(String[] args) {
-        InnerPattern10 p=new InnerPattern10();
+        InnerPattern10 p = new InnerPattern10();
         p.patterns(4);
-        p.invertedPatterns(4);
+
     }
 }
